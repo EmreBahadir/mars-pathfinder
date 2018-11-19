c8a1e8740f9af2b3f35b6b3bffb4a83d8c65d2a3
EmreBahadir <emrebahadir123@gmail.com>
2018-11-19 21:58:31 +0300

    create robot class

---
+++ Robot.cs
@@ -0,0 +1,22 @@
+﻿namespace MarsPathFinder
+{
+    class Robot
+    {
+        Point currentPoint;
+
+        public Robot(Point startPoint)
+        {
+            currentPoint = startPoint;
+        }
+
+        public void Move()
+        {
+
+        }
+
+        public void Rotate(char side)
+        {
+
+        }
+    }
+}
